aa50a2e7583b2c0f404789fa4b070895364a903e
辉哥 <995631664@qq.com>
2025-04-04 22:33:23 +0800

    Django app - part 3

--- 02_Django学习/mysite/polls/tests.py
+++ 02_Django学习/mysite/polls/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from .models import Question
+from django.utils import timezone
+
+q = Question(question_text="What's new?", pub_date=timezone.now())
+q.save()
+
+q = Question(question_text="Where it is?", pub_date=timezone.now())
+q.save()
+
+q = Question(question_text="How to do it?", pub_date=timezone.now())
+q.save()
+
+q = Question(question_text="What can I do?", pub_date=timezone.now())
+q.save()
